422aa7919fb7fd027d15350c49006c6d43809b83
Benoit <fbenoit2077@gmail.com>
2024-01-23 22:24:28 -0500

    deleteed comments and added more functions to server.js

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -7,7 +7,7 @@ INSERT INTO role (title, salary, department_id) VALUES
 ('Marketing Coordinator', 60000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
-('John', 'Doe', 1, NULL), -- Assuming the 'HR Manager' role has an ID of 1
-('Jane', 'Smith', 2, NULL), -- Assuming 'Sales Representative' has an ID of 2
-('Emily', 'Johnson', 3, 1), -- Assuming 'Software Engineer' has an ID of 3 and reports to John Doe
-('Mike', 'Brown', 4, 1); -- Assuming 'Marketing Coordinator' has an ID of 4 and reports to John Doe
+('John', 'Doe', 1, NULL), 
+('Jane', 'Smith', 2, NULL), 
+('Emily', 'Johnson', 3, 1), 
+('Mike', 'Brown', 4, 1); 
